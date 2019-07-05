cc1b90cce9b791b67f0db16157d4ebd119f4343d
Eddy Wong <784550567@qq.com>
2019-07-05 14:10:11 +0800

    first

---
+++ DNHeadLineAdSDK/DNHeadLineAdSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DNHeadLineAdSDK
+//
+//  Created by 动能无限 on 2019/7/3.
+//  Copyright © 2019 动能无限. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DNHeadLineAdSDK/DNHeadLineAdSDK/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DNHeadLineAdSDK
+//
+//  Created by 动能无限 on 2019/7/3.
+//  Copyright © 2019 动能无限. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
